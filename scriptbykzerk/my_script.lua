1514decff74aeb6d6a4a7fff7d0814b47a7f6013
kzerk <egorkuz1408@gmail.com>
2023-08-28 21:11:15 +0400

    Delete spawn point

--- scriptbykzerk/my_script.lua
+++ scriptbykzerk/my_script.lua
@@ -1,6 +1,4 @@
-local spawnX, spawnY, spawnZ = 0, 0, 10
 function joinHandler()
-	spawnPlayer(source, spawnX, spawnY, spawnZ)
 	fadeCamera(source, true)
 	setCameraTarget(source, source)
 	outputChatBox("Hello World!", source)
